8b99ed687228b97dc2129dea0c7f2ab8da579c69
baketuman <baketuman@yhoo.co.jp>
2025-10-24 23:54:05 +0900

    fix(management): import get_user_model from django.contrib.auth and minor refactor

--- expenses/management/commands/superuser.py
+++ expenses/management/commands/superuser.py
@@ -1,13 +1,13 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import get_user_model
+from django.contrib.auth import get_user_model
 from django.conf import settings
 
-user = get_user_model()
+User = get_user_model()
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if not user.objects.filter(username='idc_user').exists():
-            user.objects.create_superuser(
+        if not User.objects.filter(username='idc_user').exists():
+            User.objects.create_superuser(
                 username='idc_user',
                 password='123',
                 email=''
